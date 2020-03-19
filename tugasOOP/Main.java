e366d6dc884f82608a6b144d03b783f9b8e93809
joyouripe1 <56066788+joyouripe1@users.noreply.github.com>
2020-03-19 20:15:40 +0700

    Add files via upload

---
+++ tugasOOP/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        //membuat objek
+        Mahasiswa mhs = new Mahasiswa();
+
+        //memberi nilai atau value
+        mhs.nama = "Sammal";
+        mhs.kelas = "4A";
+        mhs.nim = "18090012";
+
+        //memanggil data
+        System.out.println("Nama: "+mhs.nama);
+        System.out.println("Kelas: "+mhs.kelas);
+        System.out.println("NIM: "+mhs.nim);
+        
+        
+        
+    }
+}
+
